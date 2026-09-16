7950929ff2c3902e16a390c33db667428690b118
Madhur <madhurchouhan7@gmail.com>
2026-09-16 15:02:00 +0530

    feat: update environment variable for API URL and enhance Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  envPrefix: ['VITE_', 'MY_'],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
